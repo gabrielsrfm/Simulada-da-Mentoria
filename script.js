258c5088279aa4fe48b4582b134843b737a98ddb
Gabriel_dev <gabrielserafim401@gmail.com>
2026-04-21 18:51:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("form-inscricao")
+  .addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    alert("✅ Inscrição confirmada! Obrigado pela participação.");
+  });
